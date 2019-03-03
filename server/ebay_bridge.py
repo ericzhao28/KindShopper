72fa460e6d9c0a86ea42970d7a4e67e33c3f214d
Eric Zhao <ericlzhao28@gmail.com>
2019-03-02 22:55:55 -0800

    Working charity filter

--- server/ebay_bridge.py
+++ server/ebay_bridge.py
@@ -1,31 +1,42 @@
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.trading import Connection as Trading
-from ebaysdk.shopping import Connection as Shopping
+# from ebaysdk.shopping import Connection as Shopping
 from config import EBAY_APP_ID, EBAY_DEV_ID, EBAY_CERT_ID, EBAY_TOKEN
+from pprint import pprint
 
 
-finding_api = Finding(domain='svcs.sandbox.ebay.com', appid=EBAY_APP_ID, config_file=None)
-shopping_api = Shopping(domain='svcs.sandbox.ebay.com', appid=EBAY_APP_ID, config_file=None)
-trading_api = Trading(domain='api.sandbox.ebay.com', appid=EBAY_APP_ID, devid=EBAY_DEV_ID,
+finding_api = Finding(domain='svcs.ebay.com', appid=EBAY_APP_ID, config_file=None)
+# shopping_api = Shopping(domain='svcs.ebay.com', version="671", appid=EBAY_APP_ID, config_file=None)
+trading_api = Trading(domain='api.ebay.com', appid=EBAY_APP_ID, devid=EBAY_DEV_ID,
                       certid=EBAY_CERT_ID, token=EBAY_TOKEN, config_file=None)
 
+# def shopping_api(parsed):
+#    response = shopping_api.execute('FindPopularItems', {'QueryKeywords': 'Python'})
+#    print(response.dict())
+#    print(response.reply)
 
-def attempt_connect():
+cache = {}
+
+def get_charity_name(charity_id):
+    if charity_id in cache:
+        return cache[charity_id]
+
     response = trading_api.execute('GetUser', {})
     print(response.dict())
-    print(response.reply)
 
+    trading_api.execute('GetCharities', {'CharityID': charity_id})
 
-def find_charitable(parsed):
-    response = shopping_api.execute('FindPopularItems', {'QueryKeywords': 'Python'})
-    print(response.dict())
-    print(response.reply)
+    name = finding_api.response.reply.Charity.Name
+    cache[charity_id] = name
+    return name
 
 
 def find_products(parsed):
-    finding_api.execute('findItemsAdvanced', {
+    finding_api.execute("findItemsAdvanced", {
         'keywords': 'red t-shirt',
         'itemFilter': [
+            {'name': 'CharityOnly',
+             'value': 'True'},
         ],
         'paginationInput': {
             'entriesPerPage': '5',
@@ -35,14 +46,16 @@ def find_products(parsed):
     })
 
     dictstr = finding_api.response.dict()
-
     if "item" not in dictstr["searchResult"]:
         return []
 
     for item in dictstr['searchResult']['item']:
-        # print(dictstr['searchResult']['item'])
+        pprint(dictstr['searchResult']['item'])
         print('Title: {}'.format(item['title']))
         print('URL: {}'.format(item['viewItemURL']))
+        if "charityId" in item:
+            print(item["charityId"])
+            print(get_charity_name(item["charityId"]))
         print('Price: {}'.format(item["sellingStatus"]["currentPrice"]["value"] + " "
                                  + item["sellingStatus"]["currentPrice"]["_currencyId"]))
 
@@ -51,4 +64,4 @@ def find_products(parsed):
 
 
 find_products(None)
-find_charitable(None)
+# find_charitable(None)
